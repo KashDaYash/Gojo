577da69ca8de38fa64166a4a042cdb1a2150f35e
KashDaYash <KashDaYash@gmail.com>
2024-12-03 22:53:41 +0530

    update

---
+++ Gojo/__init__.py
@@ -0,0 +1,27 @@
+from Gojo.core.bot import Gojo
+from Gojo.core.dir import dirr
+from Gojo.core.git import git
+from Gojo.core.userbot import Userbot
+from Gojo.misc import dbb, heroku
+from pyrogram import Client
+from SafoneAPI import SafoneAPI
+from .logging import LOGGER
+
+dirr()
+git()
+dbb()
+heroku()
+
+app = Gojo()
+api = SafoneAPI()
+userbot = Userbot()
+
+from .platforms import *
+
+Apple = AppleAPI()
+Carbon = CarbonAPI()
+SoundCloud = SoundAPI()
+Spotify = SpotifyAPI()
+Resso = RessoAPI()
+Telegram = TeleAPI()
+YouTube = YouTubeAPI()
---
+++ Gojo/plugins/admins/stop.py
@@ -0,0 +1,23 @@
+from pyrogram import filters
+from pyrogram.types import Message
+
+from Gojo import app
+from Gojo.core.call import Gojo
+from Gojo.utils.database import set_loop
+from Gojo.utils.decorators import AdminRightsCheck
+from Gojo.utils.inline import close_markup
+from config import BANNED_USERS
+
+
+@app.on_message(
+    filters.command(["end", "stop", "cend", "cstop"], prefixes=["/", "!", "%", ",", "", ".", "@", "#"]) & filters.group & ~BANNED_USERS
+)
+@AdminRightsCheck
+async def stop_music(cli, message: Message, _, chat_id):
+    if not len(message.command) == 1:
+        return
+    await Gojo.stop_stream(chat_id)
+    await set_loop(chat_id, 0)
+    await message.reply_text(
+        _["admin_5"].format(message.from_user.mention), reply_markup=close_markup(_)
+    )
